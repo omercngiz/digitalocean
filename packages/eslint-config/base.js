c036da5f1758680cae6f35f49c68f5c8ab1f7d53
Omer Cengiz <omer.cengiz-34@hotmail.com>
2026-04-09 14:24:46 +0300

    add ESLint and prettier formating

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,20 +1,22 @@
 import js from "@eslint/js";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      turbo: turboPlugin,
-    },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
-  },
-  {
-    ignores: ["node_modules/", "dist/", ".next/", ".turbo/"],
-  },
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		plugins: {
+			turbo: turboPlugin,
+		},
+		rules: {
+			"turbo/no-undeclared-env-vars": "warn",
+		},
+	},
+	prettierConfig,
+	{
+		ignores: ["node_modules/", "dist/", ".next/", ".turbo/"],
+	},
 ];
